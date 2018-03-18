216b176ad7f656cd1bcaec0351a07ff829ae020b
view z370 <648764190@qq.com>
2018-03-18 23:10:37 +0800

    add effect

--- src/views/Base.vue
+++ src/views/Base.vue
@@ -1,13 +1,13 @@
 <template>
   <el-container class="base">
     <el-aside class="base-aside" width="200px">
-      <div class="base-logo"></div>
+      <div class="base-logo">EL-PRO</div>
       <div class="base-menu">
         <div
           class="base-menu-item"
           v-for="parent in BaseMenuConfig"
           :key="parent.id"
-          @click="onMenuClick(parent)">
+          @click="(e) => onMenuClick(e, parent)">
           <span>
             {{parent.title}}
           </span>
@@ -41,9 +41,33 @@ export default {
           break
       }
     },
-    onMenuClick(parent) {
+    onMenuClick(e, parent) {
+      this.addWave(e)
       this.$router.push({
         name: parent.link
+      })
+    },
+    addWave(e) {
+      let target = e.target
+      let style = getComputedStyle(target)
+      let width = parseInt(style.width) + parseInt(style.paddingLeft) + parseInt(style.paddingRight)
+      let height = parseInt(style.height) + parseInt(style.paddingTop) + parseInt(style.paddingBottom)
+      let size = width > height ? width : height
+      let $el = document.createElement('div')
+      $el.classList.add('wave')
+      $el.style.width = 0
+      $el.style.height = size + 'px'
+      $el.style.opacity = 0.3
+      $el.addEventListener('transitionend', () => {
+        if ($el && $el.parentNode) {
+          $el.parentNode.removeChild($el)
+        }
+      })
+      target.appendChild($el)
+      this.$nextTick(() => {
+        $el.style.width = size + 'px'
+        $el.style.height = size + 'px'
+        $el.style.opacity = 0
       })
     }
   }
